cc435b842024848572dcf731ac2e1a018aae5476
Sitraka Forler <49253764+Sitraka17@users.noreply.github.com>
2023-03-25 23:59:25 +0100

    bro i hain t einstein

---
+++ App.py
@@ -0,0 +1,43 @@
+import streamlit as st
+
+# Title
+st.title("My Streamlit App")
+
+# Header
+st.header("This is a header")
+
+# Subheader
+st.subheader("This is a subheader")
+
+# Text
+st.write("This is some text.")
+
+# Markdown
+st.markdown("This is some **bold** text.")
+
+# Dataframe
+import pandas as pd
+df = pd.DataFrame({
+  'column 1': [1, 2, 3, 4],
+  'column 2': ['a', 'b', 'c', 'd']
+})
+st.write(df)
+
+# Plot
+import matplotlib.pyplot as plt
+import numpy as np
+x = np.linspace(0, 10, 100)
+y = np.sin(x)
+plt.plot(x, y)
+st.pyplot(plt)
+
+# Checkbox
+if st.checkbox("Show/hide"):
+  st.write("You checked the checkbox")
+
+# Selectbox
+option = st.selectbox(
+  "Select an option",
+  ["Option 1", "Option 2", "Option 3"]
+)
+st.write("You selected:", option)
